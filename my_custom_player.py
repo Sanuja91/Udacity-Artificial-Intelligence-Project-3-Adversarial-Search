e1ba53e7a49a702be35b0db26f024a0e3b12dbb8
Sanuja Cooray <sanujac@live.com>
2020-03-15 15:56:50 +0530

    Unexpected error:<class 'isolation.StopSearch'> Error

--- my_custom_player.py
+++ my_custom_player.py
@@ -1,4 +1,5 @@
-import random
+import random, sys
+from time import time
 import numpy as np
 from copy import copy, deepcopy
 from sample_players import DataPlayer
@@ -21,11 +22,15 @@ class Node:
         """Checks if leaf node"""
         return len(self.unvisited_actions) != 0
 
+    def has_children(self):
+        """Checks if leaf node"""
+        return len(self.children) > 0
+
     def is_terminal(self):
         """Checks if terminal state"""
         return self.state.terminal_test()
 
-    def value(self):
+    def get_value(self):
         """Gets value of node"""
         if self.visits == 0:
             return 0
@@ -33,11 +38,12 @@ class Node:
 
     def expand(self):
         """Expands existing leaf node"""
-        action = random.choice(self.unvisited_actions)
-        next_state = self.state.result(action)
-        self.unvisited_actions.remove(action)
-        node = Node(state=next_state, parent=self, parent_action=action)
-        self.children[action] = node
+        while len(self.unvisited_actions) > 0:
+            action = random.choice(self.unvisited_actions)
+            next_state = self.state.result(action)
+            self.unvisited_actions.remove(action)
+            node = Node(state=next_state, parent=self, parent_action=action)
+            self.children[action] = node
         return node
 
 
@@ -51,13 +57,15 @@ class MCTS:
     you expand it without using its rollout value
     """
 
-    def __init__(self, simulations, player_id, exploration_factor=1.4, expert_factor=2.0, expansion_threshold=100, rollout_times=10):
+    def __init__(self, simulations, player_id, exploration_factor=1.4, expert_factor=2.0, expansion_threshold=100, rollout_times=10, simulation_time = 4.9):
         self.simulations = simulations
         self.exploration_factor = exploration_factor
         self.expert_factor = expert_factor
         self.expansion_threshold = expansion_threshold
         self.rollout_times = rollout_times
         self.player_id = player_id
+        self.simulation_time = simulation_time
+        self.time = time()
 
     def run(self, state: isolation):
         root = Node(state)
@@ -67,24 +75,50 @@ class MCTS:
             node = root
             search_path = [node]
 
-            while not node.is_terminal():
-				
-                if node.visits >= self.expansion_threshold and node.is_expandable():
-                    print("NODE VISITS", node.visits)
-                    print('Expanding Node')
-                    node = node.expand()
-                    search_path.append(node)
-                elif node.visits == 0:
-                    print('Rolling Out Node')
-                    node = self.rollout(node, search_path)
-                    search_path.append(node)
-                else:
+            while not node.is_terminal() and time() - self.time < self.simulation_time * 0.8:
+
+                if node.has_children() and node.visits > 0:
                     print('Selecting Node')
                     node = self.select_next_best_node(node)
                     search_path.append(node)
+                elif node.visits >= self.expansion_threshold and node.is_expandable():
+                    print('Expanding Node')
+                    node = node.expand()
+                    search_path.append(node)
+                else:
+                    print('Rolling Out Node')
+                    node = self.rollout(node, search_path)
+                    search_path.append(node)
 
         best_next_node = self.select_next_best_node(root)
+        print('BEST ACTION', best_next_node)
         return best_next_node.parent_action
+
+    def rollout(self, node, search_path):
+        """Rollouts the states randomly to find approximate value of the node"""
+        for action in node.state.actions():
+            search_path_ = copy(search_path)
+            search_path_.append(node)
+            rolled_out_times = 0
+            while self.rollout_times > rolled_out_times:
+                state = deepcopy(node.state)
+                count = 0
+                while not state.terminal_test():
+                    count += 1
+                    state = state.result(random.choice(state.actions()))
+                    print('SUB LOOP COUNT', count, 'NEXT ACTIONS', len(state.actions()), time() - self.time)
+                print('EXIT SUB LOOP')
+                value = state.utility(self.player_id)
+                if value == float('inf'):
+                    value = 1
+                elif value == float('-inf'):
+                    value = -1
+                else:
+                    value = 0                    
+                self.backpropogate(search_path_, value)
+                rolled_out_times += 1
+                print('ROLLED OUT TIMES', rolled_out_times)
+        return node
 
     def select_next_best_node(self, node):
         """Picks next child node based on highest value"""
@@ -93,35 +127,19 @@ class MCTS:
         for child_key in children.keys():
             child = children[child_key]
             # value for exploiting the child path
-            exploitation_value = child.value()
+            exploitation_value = child.get_value()
             # value for exploring the child path
-            exploration_value = self.exploration_factor * \
-                np.log(node.visits) / child.visits
+            print('EXP FACTOR', self.exploration_factor, 'NODE VISITS', node.visits, 'LOG VISITS', np.log(node.visits), 'CHILD VISITS', child.visits)
+            exploration_value = self.exploration_factor * np.log(node.visits) / child.visits
             # value for expert knowledge
             # expert_value = self.expert_factor * expert_knowledge / (child.visits + 1)
 
             # children_values[child_key] = exploitation_value + exploration_value + expert_value
-            print("CHILD KEY", child_key, "EXPLOIT",
-                  exploitation_value, "EXPLORE", exploration_value)
+            print("CHILD KEY", child_key, "EXPLOIT", exploitation_value, "EXPLORE", exploration_value)
             children_values[child_key] = exploitation_value + exploration_value
-            print("CHILDREN VALUES", children_values)
         selected_child_key = max(children_values, key=children_values.get)
+        print('SELECTED CHILD', selected_child_key)
         return children[selected_child_key]
-
-    def rollout(self, node, search_path):
-        """Rollouts the states randomly to find approximate value of the node"""
-        for action in node.state.actions():
-            state = node.state
-            search_path_ = copy(search_path)
-            search_path_.append(node)
-            rolled_out_times = 0
-            while self.rollout_times > rolled_out_times:
-                while not state.terminal_test():
-                    state = state.result(random.choice(state.actions()))
-                value = state.utility(self.player_id)
-                self.backpropogate(search_path_, value)
-                rolled_out_times += 1
-        return node
 
     def backpropogate(self, search_path, value):
         for node in reversed(search_path):
@@ -167,11 +185,14 @@ class CustomPlayer(DataPlayer):
         # EXAMPLE: choose a random move without any search--this function MUST
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
-        mcts = MCTS(100, self.player_id)
-        if state.ply_count < 2:
-            self.queue.put(random.choice(state.actions()))
-        else:
-            self.queue.put(mcts.run(state))
+        try:
+            mcts = MCTS(100, self.player_id)
+            if state.ply_count < 2:
+                self.queue.put(random.choice(state.actions()))
+            else:
+                self.queue.put(mcts.run(state))
+        except:
+            print("Unexpected error:"+str(sys.exc_info()[0]))
 
     def alpha_beta_search(self, state, depth):
         def min_value(state, depth, alpha, beta):
